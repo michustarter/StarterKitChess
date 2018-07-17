e45ec10b0a40ca035cf3979a1abc585625592c89
michustarter <michal.ratajczyk@capgemini.com>
2018-07-17 11:55:22 +0200

    * add implementation for method isMoveToDestination in class Knight * remove comments from King class.

--- ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/King.java
+++ ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/King.java
@@ -9,11 +9,6 @@ public class King implements PieceOnBoard {
 	@Override
 	public boolean isMoveToDestination(Coordinate from, Coordinate to, Board board) throws InvalidMoveException {
 
-		/*
-		 * TYLKO TO PONI¯EJ SPR: tutaj pole TO moze byæ jedynie +-1 oddalone od
-		 * pola from, inna figura moze stac (juz spr czy inny kolor na pewno w
-		 * Validation)
-		 */
 		int yFrom = from.getY();
 		int xFrom = from.getX();
 		int yTo = to.getY();
@@ -25,7 +20,6 @@ public class King implements PieceOnBoard {
 				isPossibility = true;
 				return isPossibility;
 			}
-
 		} else {
 			isPossibility = false;
 		}
--- ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/Knight.java
+++ ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/Knight.java
@@ -9,7 +9,23 @@ public class Knight implements PieceOnBoard {
 	@Override
 	public boolean isMoveToDestination(Coordinate from, Coordinate to, Board board) throws InvalidMoveException {
 
-		return false;
+		int yFrom = from.getY();
+		int xFrom = from.getX();
+		int yTo = to.getY();
+		int xTo = to.getX();
+		boolean isPossibility = false;
 
+		if ((xFrom - 1) == xTo || (xFrom + 1) == xTo) {
+			if ((yFrom - 2) == yTo || (yFrom + 2) == yTo) {
+				isPossibility = true;
+				return isPossibility;
+			}
+		} else if ((xFrom - 2) == xTo || (xFrom + 2) == xTo) {
+			if ((yFrom - 1) == yTo || (yFrom + 1) == yTo) {
+				isPossibility = true;
+				return isPossibility;
+			}
+		}
+		return isPossibility;
 	}
 }
